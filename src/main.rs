7f9c222cccb8f945cfdd79dfcddcbd463e4832dd
Jared Kerim <jared.l.kerim@gmail.com>
2018-07-17 15:31:59 -0400

    Refactor into flat weights vector

--- src/main.rs
+++ src/main.rs
@@ -4,84 +4,70 @@ fn random_weight() -> f64 {
     (rand::random::<f64>() * 2.0) - 1.0
 }
 
-fn random_weights(size: usize) -> Vec<f64> {
-    (0..size).map(|_| random_weight()).collect()
+fn random_weights(n: usize) -> Vec<f64> {
+    (0..n).map(|_| random_weight()).collect()
 }
 
 fn sigmoid(x: f64) -> f64 {
     1.0 / (1.0 + (-x).exp())
 }
 
-#[derive(Debug)]
-struct Neuron {
+fn perceptron(inputs: &Vec<f64>, weights: Vec<f64>) -> f64 {
+    let zipped = inputs.iter().zip(weights.iter());
+    let sum: f64 = zipped.map(|(input, weight)| input * weight).sum();
+    sigmoid(sum)
+}
+
+fn feed_layer(
+    inputs: &Vec<f64>,
     weights: Vec<f64>,
-}
+    num_inputs: usize,
+    num_outputs: usize,
+) -> Vec<f64> {
+    let mut outputs: Vec<f64> = Vec::new();
 
-impl Neuron {
-    fn new(num_weights: usize) -> Self {
-        Self {
-            weights: random_weights(num_weights),
-        }
+    for output_i in 0..num_outputs {
+        let start = output_i * num_inputs;
+        let end = (output_i + 1) * num_inputs;
+        let output = perceptron(inputs, weights[start..end].to_vec());
+        outputs.push(output);
     }
 
-    fn feed(&self, inputs: &Vec<f64>) -> f64 {
-        let zipped = inputs.iter().zip(self.weights.iter());
-        let sum: f64 = zipped.map(|(input, weight)| input * weight).sum();
-        sigmoid(sum)
-    }
-}
-
-#[derive(Debug)]
-struct Layer {
-    neurons: Vec<Neuron>,
-}
-
-impl Layer {
-    fn new(num_inputs: usize, num_outputs: usize) -> Self {
-        Layer {
-            neurons: (0..num_outputs).map(|_| Neuron::new(num_inputs)).collect(),
-        }
-    }
-
-    fn feed(&self, inputs: &Vec<f64>) -> Vec<f64> {
-        let mut output: Vec<f64> = Vec::new();
-
-        for neuron in self.neurons.iter() {
-            let neuron_output = neuron.feed(inputs);
-            output.push(neuron_output);
-        }
-
-        output
-    }
+    outputs
 }
 
 #[derive(Debug)]
 struct Network {
-    layer1: Layer,
-    layer2: Layer,
+    inputs: usize,
+    hidden: usize,
+    outputs: usize,
+    weights: Vec<f64>,
 }
 
 impl Network {
-    fn new(num_inputs: usize, num_hidden: usize, num_outputs: usize) -> Self {
+    fn new_random(inputs: usize, hidden: usize, outputs: usize) -> Self {
         Network {
-            layer1: Layer::new(num_inputs, num_hidden),
-            layer2: Layer::new(num_hidden, num_outputs),
+            inputs: inputs,
+            hidden: hidden,
+            outputs: outputs,
+            weights: random_weights((inputs * hidden) + (hidden * outputs)),
         }
     }
 
     fn feed(&self, inputs: &Vec<f64>) -> Vec<f64> {
-        self.layer2.feed(&self.layer1.feed(inputs))
+        let layer_cutoff = self.inputs * self.hidden;
+        let layer1_weights = self.weights[..layer_cutoff].to_vec();
+        let layer1_output = feed_layer(inputs, layer1_weights, self.inputs, self.hidden);
+        let layer2_weights = self.weights[layer_cutoff..].to_vec();
+        feed_layer(&layer1_output, layer2_weights, self.hidden, self.outputs)
     }
 }
 
 fn main() {
-    let neuron = Neuron::new(3);
-    let layer = Layer::new(2, 3);
-    let network = Network::new(2, 3, 1);
+    let network = Network::new_random(2, 3, 1);
     let input = vec![1.0, 1.0];
+    let weights = network.weights[..(network.inputs * network.hidden)].to_vec();
     let output = network.feed(&input);
-    println!("Hello, neuron: {:?}", neuron);
-    println!("Hello, layer: {:?}", layer);
     println!("Hello, network: {:?}", network);
     println!("\nInput: {:?}", input);
     println!("\nOutput: {:?}", output);
