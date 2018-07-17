8efc30232b6f6b2ce67abb5fa1cab0c60e4d8b03
Jared Kerim <jared.l.kerim@gmail.com>
2018-07-17 15:33:06 -0400

    Remove unused variable

--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,6 @@ impl Network {
 fn main() {
     let network = Network::new_random(2, 3, 1);
     let input = vec![1.0, 1.0];
-    let weights = network.weights[..(network.inputs * network.hidden)].to_vec();
     let output = network.feed(&input);
     println!("Hello, network: {:?}", network);
     println!("\nInput: {:?}", input);
