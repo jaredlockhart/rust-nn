95731c8d35b771358b5ae370b2140400fed406f4
Jared Kerim <jared.l.kerim@gmail.com>
2018-07-17 17:16:26 -0400

    More Rusty, More Fasty

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 extern crate rand;
+extern crate rayon;
+
+use rayon::prelude::*;
 
 fn random_weight() -> f64 {
     (rand::random::<f64>() * 2.0) - 1.0
@@ -12,35 +15,27 @@ fn sigmoid(x: f64) -> f64 {
     1.0 / (1.0 + (-x).exp())
 }
 
-fn perceptron(inputs: &Vec<f64>, weights: Vec<f64>) -> f64 {
-    let zipped = inputs.iter().zip(weights.iter());
-    let sum: f64 = zipped.map(|(input, weight)| input * weight).sum();
-    sigmoid(sum)
+fn perceptron(inputs: &Vec<f64>, weights: &[f64]) -> f64 {
+    sigmoid(
+        inputs
+            .iter()
+            .zip(weights.iter())
+            .map(|(input, weight)| input * weight)
+            .sum(),
+    )
 }
 
-fn feed_layer(
-    inputs: &Vec<f64>,
-    weights: Vec<f64>,
-    num_inputs: usize,
-    num_outputs: usize,
-) -> Vec<f64> {
-    let mut outputs: Vec<f64> = Vec::new();
-
-    for output_i in 0..num_outputs {
-        let start = output_i * num_inputs;
-        let end = (output_i + 1) * num_inputs;
-        let output = perceptron(inputs, weights[start..end].to_vec());
-        outputs.push(output);
-    }
-
-    outputs
+fn feed_layer(inputs: &Vec<f64>, weights: &[f64]) -> Vec<f64> {
+    weights
+        .chunks(inputs.len())
+        .map(|input_weights| perceptron(inputs, input_weights))
+        .collect()
 }
 
 #[derive(Debug)]
 struct Network {
     inputs: usize,
     hidden: usize,
-    outputs: usize,
     weights: Vec<f64>,
 }
 
@@ -49,17 +44,14 @@ impl Network {
         Network {
             inputs: inputs,
             hidden: hidden,
-            outputs: outputs,
             weights: random_weights((inputs * hidden) + (hidden * outputs)),
         }
     }
 
     fn feed(&self, inputs: &Vec<f64>) -> Vec<f64> {
         let layer_cutoff = self.inputs * self.hidden;
-        let layer1_weights = self.weights[..layer_cutoff].to_vec();
-        let layer1_output = feed_layer(inputs, layer1_weights, self.inputs, self.hidden);
-        let layer2_weights = self.weights[layer_cutoff..].to_vec();
-        feed_layer(&layer1_output, layer2_weights, self.hidden, self.outputs)
+        let layer1_output = feed_layer(inputs, &self.weights[..layer_cutoff]);
+        feed_layer(&layer1_output, &self.weights[layer_cutoff..])
     }
 }
 
